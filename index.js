97e3d3455158f07feb8da3d245dd3e582438d079
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-11-21 11:02:58 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    {year: "2020", result: "L"},
+    {year: "2021", result: "W"},
+    {year: "2022", result: "N/A"}
+]
+
+const superbowlWin = (record) => {
+    
+    const result = record.find((l) => {
+        if(l.result === "W") {
+            return true
+        } else { return false }
+    })
+
+    if(result) { return result.year } else { return undefined }
+
+}
